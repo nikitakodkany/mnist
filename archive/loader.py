0d7458c5bd2fdddba52537db3a913256b501f035
nikitakodkany <nikitahkodkany@gmail.com>
2020-06-19 04:57:46 +0530

    dataset loader

--- archive/loader.py
+++ archive/loader.py
@@ -6,10 +6,8 @@ def load_data(prefix, folder):
 
     data = np.fromfile(folder + "/" + prefix +'-images.idx3-ubyte', dtype = 'ubyte')
     magicBytes, nImages, width, height = np.frombuffer(data[:nMetaDataBytes].tobytes(), intType)
-    # data = data[nMetaDataBytes:].astype('int32').reshape(nImages, width, height)
-    # data = data[nMetaDataBytes:].astype(dtype='float32').reshape([nImages, width, height])
+    data = data[nMetaDataBytes:].astype(dtype='float32').reshape([nImages, width, height])
 
-    print(data.shape)
     labels = np.fromfile(folder + "/" + prefix + '-labels.idx1-ubyte', dtype ='ubyte')[2 * intType.itemsize:]
 
     return data, labels
@@ -17,6 +15,5 @@ def load_data(prefix, folder):
 def toReturn():
     traininImages, trainingLabels = load_data("train", "dataset")
     testImages, testLabels = load_data("t10k", "dataset")
-    # print(traininImages.shape, trainingLabels.shape)
-
+    
     return traininImages, trainingLabels, testImages, testLabels
