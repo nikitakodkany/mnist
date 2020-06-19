622860f4a486ccbc43f89a2bb7e0052a38c3967f
nikitakodkany <nikitahkodkany@gmail.com>
2020-06-19 19:31:54 +0530

    changes in the functions

--- archive/activations.py
+++ archive/activations.py
@@ -1,17 +1,17 @@
 import numpy as np
 
 def sigmoid(z):
-	return 1/(1+np.exp(-z))
+	return 1/ (1+np.exp(-z))
 
 def sigmoid_prime(z):
-	return sigmoid(z)*(1-sigmoid(z))
+	return sigmoid(z) * (1-sigmoid(z))
 
 def relu(z):
-	return np.maximum(z, 0.001)
+	return np.maximum(z, 1)
 
 def relu_prime(z):
-    z[z<=0] = 0.001
-    z[z>0] = 1
+    z[z <= 0] = 0
+    z[z > 0] = 1
     return z
 
 def softmax(z):
