6baeb2432dd6ac31866a92f06a701dd1ccacce0a
nikitakodkany <nikitahkodkany@gmail.com>
2020-06-20 02:55:26 +0530

    updated loader acc. to dataset

--- archive/loader.py
+++ archive/loader.py
@@ -4,11 +4,11 @@ def load_data(prefix, folder):
     intType = np.dtype('int32').newbyteorder('>')
     nMetaDataBytes = 4 * intType.itemsize
 
-    data = np.fromfile(folder + "/" + prefix +'-images.idx3-ubyte', dtype = 'ubyte')
+    data = np.fromfile(folder + "/" + prefix +'-images-idx3-ubyte', dtype = 'ubyte')
     magicBytes, nImages, width, height = np.frombuffer(data[:nMetaDataBytes].tobytes(), intType)
     data = data[nMetaDataBytes:].astype(dtype='float32').reshape([nImages, width, height])
 
-    labels = np.fromfile(folder + "/" + prefix + '-labels.idx1-ubyte', dtype ='ubyte')[2 * intType.itemsize:]
+    labels = np.fromfile(folder + "/" + prefix + '-labels-idx1-ubyte', dtype ='ubyte')[2 * intType.itemsize:]
 
     return data, labels
 
